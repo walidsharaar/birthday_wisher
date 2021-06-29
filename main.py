3fd8daad03d7e68bd47af9412750dbe05c629cf0
Sharaar <walidsharar@gmail.com>
2021-06-29 15:51:36 +0200

    Send email

---
+++ main.py
@@ -0,0 +1,14 @@
+import smtplib
+
+my_email ="mmytest779@gmail.com"
+my_password="1234567@abc%$"
+
+with smtplib.SMTP("smtp.gmail.com") as new_connection:
+
+    # TLS Transport Layer Security which secure the connection
+    new_connection.starttls()
+    new_connection.login(user=my_email,password=my_password)
+    new_connection.sendmail(from_addr=my_email,
+                            to_addrs="abcv@gmail.com" ,
+                            msg="Subject:Hello\n\n This is body of sample email"
+                            )
